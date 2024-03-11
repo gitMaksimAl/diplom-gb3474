09f58c276c1dc4d564f507984ba802b204b50594
Maksim Aleksanin <maksim.g00gle@gmail.com>
2024-03-11 22:24:45 +0400

    chore: Added fetch function in nav.js, views not functionality

--- MrGlider/personalpage/personalapp/views.py
+++ MrGlider/personalpage/personalapp/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, Http404, JsonResponse
 from django.core.exceptions import ObjectDoesNotExist
-from django.db.models import QuerySet
+from django.db.models import QuerySet, Count
+from django.forms.models import model_to_dict
 
 from .models import User, Event, SocialMedia, Project, Skill, Certificate
 from .forms import ContactForm
@@ -39,10 +40,14 @@ def all_projects(request: HttpRequest) -> JsonResponse:
 
 def all_skills(request: HttpRequest) -> JsonResponse:
     user = User.objects.filter(active=True).first()
-    query = Skill.objects.filter(user_id=user.pk).all().query
-    query.group_by = ['name']
-    skills = QuerySet(query=query, model=Skill)
-    return JsonResponse(data={'skills': skills})
+    skills: QuerySet = (
+        Skill.objects.filter(user_id=user.pk)
+        .values('name', 'title', 'description')
+        .annotate(skill_count=Count('name'))
+        .order_by()
+    )
+    print(f"PRINT: {skills}")
+    return JsonResponse(data={'skills': skills.all()})
 
 
 def contact(request: HttpRequest) -> JsonResponse:
--- MrGlider/personalpage/static/scripts/nav.js
+++ MrGlider/personalpage/static/scripts/nav.js
@@ -1,3 +1,5 @@
+const dev_domen = 'http://localhost:8000'
+const domen = 'https://maskim-al.ru'
 function CreateContainer(id) {
     let newItem = document.createElement("div");
     newItem.id = 'category';
@@ -11,34 +13,55 @@ function GetWindow(name) {
     let modalWindows = document.getElementsByClassName('modal-window active');
     if (!document.getElementById('category')) {
         let newItem = CreateContainer(name);
-        for (let window of modalWindows) {
-            window.appendChild(newItem);
-            window.style.display = 'block';
+        for (let item of modalWindows) {
+            item.appendChild(newItem);
+            item.style.display = 'block';
         }
     }
     else {
-        for (let window of modalWindows) {
-            window.style.display = 'none';
-            window.firstChild.remove();
+        for (let item of modalWindows) {
+            item.style.display = 'none';
+            item.firstChild.remove();
         }
     }
+}
+
+function GetSkills() {
+    (async() => {
+        GetWindow('skills');
+        const targetElement = document.getElementById('skills');
+        const response = await fetch(`${dev_domen}/skills/`);
+        let skills = response.body;
+        console.log(skills);
+        for (const group of skills) {
+            targetElement.innerHTML = `<h1>${group[0].name}</h1>`;
+            group.forEach((skill) => {
+                let header = document.createElement('h3');
+                header.innerText = skill.title;
+                targetElement.appendChild(header);
+                let body = document.createElement('span');
+                body.innerText = skill.description;
+                targetElement.appendChild(body);
+            })
+        }
+    })();
 }
 
 function  GetCalendarWindow() {
     let modalWindows = document.getElementsByClassName('modal-window active');
     if (!document.getElementById('category')) {
         let newItem = CreateContainer('calendar');
-        for (let window of modalWindows) {
-            window.appendChild(newItem);
-            window.style.display = 'block';
+        for (let item of modalWindows) {
+            item.appendChild(newItem);
+            item.style.display = 'block';
         }
         GetCalendar();
         GetData();
     }
     else {
-        for (let window of modalWindows) {
-            window.style.display = 'none';
-            window.firstChild.remove();
+        for (let item of modalWindows) {
+            item.style.display = 'none';
+            item.firstChild.remove();
         }
     }
 }
